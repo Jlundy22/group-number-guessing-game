14f7e159cc530183de8c6a2abdd1682f4d58eeda
Jack Lund <jacklund920@gmail.com>
2022-04-14 12:05:50 -0500

    initial commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,9 @@ $(document).ready(handleReady);
 function handleReady() {
   console.log("jquery is loaded!")
 }
+
+function createRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+
+}
+console.log(createRandomNumber(1,26));
